18d1f103f208bb6aa1351ec8c0d22ebbc1d48672
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-11-18 15:21:28 +0700

    init project

---
+++ src/test/java/id/anggra/restapispring/RestApiSpringApplicationTests.java
@@ -0,0 +1,15 @@
+package id.anggra.restapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
